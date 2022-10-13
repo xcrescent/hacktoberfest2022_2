95bf83911812efeb24182a7216fcddeb6a8d9a43
Utkarsh Singh <64301885+utkarshsinghjadon@users.noreply.github.com>
2022-10-13 06:35:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var form = document.querySelector('#validationForm')
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     console.log("First Name : "+form.fname.value)
-    console.log("First Name : "+form.fname.value)
+    console.log("Last Name : "+form.flname.value)
     console.log("Phone Number : "+form.fphone.value)
     console.log("Email : "+form.femail.value)
     console.log("Password : "+form.fpass.value)
